d32a6ae3979b3e0a1c9b8cb546e4531855af5fe4
Fred <frederik.stonge@sonepar.ca>
2021-11-11 09:52:18 -0500

    Keep translated attributes in memory for better performance

--- src/AspNetCore.Routing.Translation/Services/RouteService.cs
+++ src/AspNetCore.Routing.Translation/Services/RouteService.cs
@@ -10,23 +10,24 @@ namespace AspNetCore.Routing.Translation.Services
 {
     internal class RouteService : IRouteService
     {
-        private readonly IActionDescriptorCollectionProvider _actionDescriptorCollectionProvider;
+        private readonly Dictionary<string, IEnumerable<TranslateAttribute>> _translatedControllers;
+        private readonly Dictionary<string, IEnumerable<TranslateAttribute>> _translatedActions;
 
         public RouteService(IActionDescriptorCollectionProvider actionDescriptorCollectionProvider)
         {
-            _actionDescriptorCollectionProvider = actionDescriptorCollectionProvider;
+            _translatedControllers = GetTranslatedControllers(actionDescriptorCollectionProvider);
+            _translatedActions = GetTranslatedActions(actionDescriptorCollectionProvider);
         }
         
         public List<ICustomTranslation> RouteRules { get; } = new List<ICustomTranslation>();
 
         public string GetControllerTranslatedValue(string controllerName, string culture)
         {
-            var controllers = GetTranslatedControllers();
-            var controller = controllers.Keys.FirstOrDefault(k =>
+            var controller = _translatedControllers.Keys.FirstOrDefault(k =>
                 k.Equals(controllerName, StringComparison.OrdinalIgnoreCase));
             if (!string.IsNullOrEmpty(controller))
             {
-                var attributes = controllers[controller];
+                var attributes = _translatedControllers[controller];
                 var attribute =
                     attributes.FirstOrDefault(a => a.Culture.Equals(culture, StringComparison.OrdinalIgnoreCase));
 
@@ -41,12 +42,11 @@ namespace AspNetCore.Routing.Translation.Services
 
         public string GetControllerName(string translatedName, string currentCulture)
         {
-            var controllers = GetTranslatedControllers();
-            if (controllers.Any(c => c.Value.Any(a =>
+            if (_translatedControllers.Any(c => c.Value.Any(a =>
                 a.Value.Equals(translatedName, StringComparison.OrdinalIgnoreCase) &&
                 a.Culture.Equals(currentCulture, StringComparison.OrdinalIgnoreCase))))
             {
-                var controller = controllers.FirstOrDefault(c => c.Value.Any(a =>
+                var controller = _translatedControllers.FirstOrDefault(c => c.Value.Any(a =>
                     a.Value.Equals(translatedName, StringComparison.OrdinalIgnoreCase) &&
                     a.Culture.Equals(currentCulture, StringComparison.OrdinalIgnoreCase)));
 
@@ -58,12 +58,11 @@ namespace AspNetCore.Routing.Translation.Services
 
         public string GetActionTranslatedValue(string controllerName, string actionName, string culture)
         {
-            var actions = GetTranslatedActions();
-            var action = actions.Keys.FirstOrDefault(k =>
+            var action = _translatedActions.Keys.FirstOrDefault(k =>
                 k.Equals($"{controllerName}/{actionName}", StringComparison.OrdinalIgnoreCase));
             if (!string.IsNullOrEmpty(action))
             {
-                var attributes = actions[action];
+                var attributes = _translatedActions[action];
                 var attribute = attributes.FirstOrDefault(a => a.Culture.Equals(culture, StringComparison.OrdinalIgnoreCase));
 
                 if (attribute != null)
@@ -77,16 +76,15 @@ namespace AspNetCore.Routing.Translation.Services
 
         public string GetActionName(string controllerName, string translatedName, string currentCulture)
         {
-            var actions = GetTranslatedActions();
-            if (actions.Any(c => c.Value.Any(a =>
+            if (_translatedActions.Any(c => c.Value.Any(a =>
                 a.Value.Equals(translatedName, StringComparison.OrdinalIgnoreCase) &&
                 a.Culture.Equals(currentCulture, StringComparison.OrdinalIgnoreCase))))
             {
                 var prefix = $"{controllerName}/";
-                var action = actions.FirstOrDefault(c => c.Key.StartsWith(prefix, StringComparison.OrdinalIgnoreCase) &&
-                                                         c.Value.Any(a =>
-                                                             a.Value.Equals(translatedName, StringComparison.OrdinalIgnoreCase) &&
-                                                             a.Culture.Equals(currentCulture, StringComparison.OrdinalIgnoreCase)));
+                var action = _translatedActions.FirstOrDefault(c => c.Key.StartsWith(prefix, StringComparison.OrdinalIgnoreCase) &&
+                                                                    c.Value.Any(a =>
+                                                                        a.Value.Equals(translatedName, StringComparison.OrdinalIgnoreCase) &&
+                                                                        a.Culture.Equals(currentCulture, StringComparison.OrdinalIgnoreCase)));
                 
                 return action.Key.Substring(prefix.Length, action.Key.Length - prefix.Length);
             }
@@ -94,11 +92,11 @@ namespace AspNetCore.Routing.Translation.Services
             return translatedName.ToLowerInvariant();
         }
 
-        private Dictionary<string, IEnumerable<TranslateAttribute>> GetTranslatedControllers()
+        private Dictionary<string, IEnumerable<TranslateAttribute>> GetTranslatedControllers(IActionDescriptorCollectionProvider provider)
         {
             var dictionary = new Dictionary<string, IEnumerable<TranslateAttribute>>();
             
-            var items = _actionDescriptorCollectionProvider.ActionDescriptors.Items
+            var items = provider.ActionDescriptors.Items
                 .OfType<ControllerActionDescriptor>()
                 .Where(c => c.ControllerTypeInfo.GetCustomAttributes(typeof(TranslateAttribute), true).Any()).ToList();
             
@@ -113,11 +111,11 @@ namespace AspNetCore.Routing.Translation.Services
             return dictionary;
         }
         
-        private Dictionary<string, IEnumerable<TranslateAttribute>> GetTranslatedActions()
+        private Dictionary<string, IEnumerable<TranslateAttribute>> GetTranslatedActions(IActionDescriptorCollectionProvider provider)
         {
             var dictionary = new Dictionary<string, IEnumerable<TranslateAttribute>>();
             
-            var items = _actionDescriptorCollectionProvider.ActionDescriptors.Items
+            var items = provider.ActionDescriptors.Items
                 .OfType<ControllerActionDescriptor>()
                 .Where(c => c.MethodInfo.GetCustomAttributes(typeof(TranslateAttribute), true).Any()).ToList();
             
